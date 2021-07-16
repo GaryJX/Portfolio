8f5b9435f11e8ff5dda8213bcca7adeac9e5ac60
Gary Xie <garyxie13@live.com>
2021-07-15 23:29:27 -0400

    Add file for ThemeToggle

---
+++ components/ThemeToggle/ThemeToggle.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const ThemeToggle: React.FC = (props) => {
+  // const { open, onClick } = props
+
+  return (
+    <div />
+    // <button
+    //   className={`${styles.hamburgerMenu} ${
+    //     open ? styles.open : styles.closed
+    //   }`}
+    //   onClick={onClick}
+    // >
+    //   <div className={styles.hamburgerLine} />
+    //   <div className={styles.hamburgerLine} />
+    //   <div className={styles.hamburgerLine} />
+    // </button>
+  )
+}
+
+export default ThemeToggle
