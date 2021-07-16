f4982cc04f25e6ecf86219a2a169c5c8c9b7fb7b
Gary Xie <garyxie13@live.com>
2021-07-15 23:41:33 -0400

    Prevent keyframe animation from running on page load

--- components/HamburgerMenu/HamburgerMenu.tsx
+++ components/HamburgerMenu/HamburgerMenu.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './HamburgerMenu.module.scss'
 
 type HamburgerMenuProps = {
@@ -8,13 +8,25 @@ type HamburgerMenuProps = {
 
 const HamburgerMenu: React.FC<HamburgerMenuProps> = (props) => {
   const { open, onClick } = props
+  const [clickedOnce, setClickedOnce] = useState(false)
+
+  const getMenuClass = () => {
+    // Used to prevent keyframe animation on page load
+    // TODO: Investigate better solution for this issue
+    if (clickedOnce) {
+      return open ? styles.open : styles.closed
+    } else {
+      return ''
+    }
+  }
 
   return (
     <button
-      className={`${styles.hamburgerMenu} ${
-        open ? styles.open : styles.closed
-      }`}
-      onClick={onClick}
+      className={`${styles.hamburgerMenu} ${getMenuClass()}`}
+      onClick={() => {
+        setClickedOnce(true)
+        onClick()
+      }}
     >
       <div className={styles.hamburgerLine} />
       <div className={styles.hamburgerLine} />
