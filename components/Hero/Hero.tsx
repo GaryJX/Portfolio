3a2c2209b6823f27e3902e0d01e3bd1abd410f9f
Gary Xie <garyxie13@live.com>
2021-07-18 12:39:48 -0400

    Finish Hero Section

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -4,9 +4,17 @@ import styles from './Hero.module.scss'
 const Hero: React.FC = () => {
   return (
     <div className={styles.hero}>
-      <h3>Hi, my name is</h3>
-      <h2>Gary Xie.</h2>
-      <h1>I build things for the web.</h1>
+      <h3 className={styles.titleSubHeader}>Hi, my name is</h3>
+      <h2 className={styles.title}>Gary Xie.</h2>
+      <h1 className={styles.subtitle}>I develop things for the web.</h1>
+      <p className={styles.copyText}>
+        {/* // TODO: Write copy text */}
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem enim
+        fuga facere, earum distinctio iusto excepturi doloremque sit impedit
+        amet? At illo, eaque voluptatum eligendi saepe quaerat vel dicta.
+        Impedit.
+      </p>
+      <button className={styles.callToAction}>View Projects</button>
     </div>
   )
 }
