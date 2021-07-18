4ff68aa3032fe25160f868e6b83141c139668135
Gary Xie <garyxie13@live.com>
2021-07-18 00:18:01 -0400

    Add alt background color

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -2,7 +2,13 @@ import React, { useState } from 'react'
 import styles from './Hero.module.scss'
 
 const Hero: React.FC = () => {
-  return <div className={styles.hero}>Hi, my name is</div>
+  return (
+    <div className={styles.hero}>
+      <h3>Hi, my name is</h3>
+      <h2>Gary Xie.</h2>
+      <h1>I build things for the web.</h1>
+    </div>
+  )
 }
 
 export default Hero
