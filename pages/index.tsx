4803914e8668cf0d82bb12cf3a2470cc1fbcbc94
Gary Xie <garyxie13@live.com>
2021-07-18 15:26:35 -0400

    Add About section skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
 import Hero from '@/components/Hero/Hero'
+import About from '@/components/About/About'
 
 export const Home: React.FC = () => {
   return (
     <main id="main">
-      <section>
+      <section id="home">
         <Hero />
       </section>
-      <section></section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="skills"></section>
+      <section id="experience"></section>
+      <section id="projects"></section>
+      <section id="contact"></section>
     </main>
   )
 }
