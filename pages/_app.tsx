faaa916d7bed9c71d331ae88b65691df9d4595d2
Gary Xie <garyxie13@live.com>
2021-07-20 19:59:22 -0400

    Add custom scrollbar styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Head from 'next/head'
 import { AppProps } from 'next/app'
 
 import Layout from '@/components/Layout/Layout'
@@ -7,31 +6,9 @@ import '@/styles/globals.scss'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <>
-      <Head>
-        <title>Gary Xie | Software Developer</title>
-        {/* <link rel="icon" href="/logo.svg" /> */}
-        {[400, 500, 600, 700].map((weight) => (
-          <link
-            key={weight}
-            rel="preload"
-            href={`/fonts/poppins-${weight}.woff2`}
-            as="font"
-            type="font/woff2"
-            crossOrigin="anonymous"
-          />
-        ))}
-        <meta
-          name="viewport"
-          content="height=device-height, width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover, shrink-to-fit=no"
-        />
-        {/* // TODO: Add meta tags for better SEO, and maybe PWA eventually? */}
-        {/* // TODO: Add Google Analytics for my production site to see how many users visit, and from what devices? */}
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   )
 }
 
---
+++ pages/_document.tsx
@@ -0,0 +1,36 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Gary Xie | Software Developer</title>
+          {/* <link rel="icon" href="/logo.svg" /> */}
+          {[400, 500, 600, 700].map((weight) => (
+            <link
+              key={weight}
+              rel="preload"
+              href={`/fonts/poppins-${weight}.woff2`}
+              as="font"
+              type="font/woff2"
+              crossOrigin="anonymous"
+            />
+          ))}
+          <meta
+            name="viewport"
+            content="height=device-height, width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, viewport-fit=cover, shrink-to-fit=no"
+          />
+          {/* // TODO: Add meta tags for better SEO, and maybe PWA eventually? */}
+          {/* // TODO: Add Google Analytics for my production site to see how many users visit, and from what devices? */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
