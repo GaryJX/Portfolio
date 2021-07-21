d06aa167151ecd40f5e3403608b3af72187fc738
Gary Xie <garyxie13@live.com>
2021-07-20 20:55:14 -0400

    Undo next config changes

--- next.config.js
+++ next.config.js
@@ -1,43 +1,43 @@
 module.exports = {
   reactStrictMode: true,
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/fonts/poppins-400.woff2',
-  //       headers: [
-  //         {
-  //           key: 'cache-control',
-  //           value: 'public, max-age=31536000, immutable',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       source: '/fonts/poppins-500.woff2',
-  //       headers: [
-  //         {
-  //           key: 'cache-control',
-  //           value: 'public, max-age=31536000, immutable',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       source: '/fonts/poppins-600.woff2',
-  //       headers: [
-  //         {
-  //           key: 'cache-control',
-  //           value: 'public, max-age=31536000, immutable',
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       source: '/fonts/poppins-700.woff2',
-  //       headers: [
-  //         {
-  //           key: 'cache-control',
-  //           value: 'public, max-age=31536000, immutable',
-  //         },
-  //       ],
-  //     },
-  //   ]
-  // },
+  async headers() {
+    return [
+      {
+        source: '/fonts/poppins-400.woff2',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/fonts/poppins-500.woff2',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/fonts/poppins-600.woff2',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+      {
+        source: '/fonts/poppins-700.woff2',
+        headers: [
+          {
+            key: 'cache-control',
+            value: 'public, max-age=31536000, immutable',
+          },
+        ],
+      },
+    ]
+  },
 }
