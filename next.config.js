404dab8d665ec056a48cdb6dc7c00c80f7fb791f
Gary Xie <garyxie13@live.com>
2021-07-20 21:00:57 -0400

    Update next config file

--- next.config.js
+++ next.config.js
@@ -3,34 +3,7 @@ module.exports = {
   async headers() {
     return [
       {
-        source: '/fonts/poppins-400.woff2',
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/fonts/poppins-500.woff2',
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/fonts/poppins-600.woff2',
-        headers: [
-          {
-            key: 'cache-control',
-            value: 'public, max-age=31536000, immutable',
-          },
-        ],
-      },
-      {
-        source: '/fonts/poppins-700.woff2',
+        source: '/fonts/:path*',
         headers: [
           {
             key: 'cache-control',
