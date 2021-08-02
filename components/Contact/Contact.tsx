cc9a2458f2c9e0ceaea7d7e0e761b508c448a204
Gary Xie <garyxie13@live.com>
2021-08-02 19:54:08 -0400

    Add Contact Me section

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -1,24 +1,27 @@
 import React, { useState } from 'react'
 import SectionHeader from '@/components/SectionHeader/SectionHeader'
+import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa'
 import styles from './Contact.module.scss'
 
+// TODO: Eventually add an EmailJS form to the Contact section
 const Contact: React.FC = () => {
   return (
     <div className={styles.contact}>
       <SectionHeader title="Contact Me" />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit ducimus
-        ad minima corporis soluta vel similique rem enim tempora incidunt,
-        consequuntur architecto a nisi quibusdam aspernatur fugiat beatae dolor.
-        Dolore excepturi ratione ipsa illum totam sunt earum ea assumenda
-        veritatis? Dolorem dignissimos est tenetur, molestias aliquam, animi
-        magni at similique cumque qui sequi sunt nobis? Officiis eos nam qui
-        placeat. Voluptatem cumque soluta fuga, eos, totam quae similique
-        perferendis mollitia, officiis provident iusto commodi deleniti
-        voluptates sed illo maiores dolores. Obcaecati, aspernatur tempore modi
-        officia laborum ad ratione numquam praesentium eos maxime! Inventore sit
-        soluta saepe totam laudantium blanditiis quis.
-      </p>
+      <div className={styles.container}>
+        <a href="https://github.com/GaryJX" target="_blank">
+          <FaGithub />
+          GaryJX
+        </a>
+        <a href="https://www.linkedin.com/in/GaryJX" target="_blank">
+          <FaLinkedin />
+          GaryJX
+        </a>
+        <a href="mailto:gary.xie@mail.utoronto.ca" target="_blank">
+          <FaEnvelope />
+          gary.xie@mail.utoronto.ca
+        </a>
+      </div>
     </div>
   )
 }
