7c106fad209d48aa25b65b1f471fc71bbebc455e
Gary Xie <garyxie13@live.com>
2021-08-02 20:02:26 -0400

    Add projects

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -2,6 +2,17 @@ import React, { useState } from 'react'
 import SectionHeader from '@/components/SectionHeader/SectionHeader'
 import styles from './Projects.module.scss'
 
+const projects = [
+  {
+    title: 'Find My IP',
+    description: 'TODO: description',
+    technologies: ['Next.js', 'React'],
+    github: 'https://github.com/GaryJX/<todo>',
+    demo: 'https://<todo>.vercel.app',
+    screenshot: '/todo/path/to/image',
+  },
+]
+
 const Projects: React.FC = () => {
   return (
     <div className={styles.projects}>
