341ac597daa78aa8925bda28225384311d6b74aa
Gary Xie <garyxie13@live.com>
2021-08-02 20:28:57 -0400

    Add todo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,11 +5,12 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <script
+          {/* // TODO: Uncomment this later to include Google Analytics (the issue is that it is getting blocked by ad blockers right now) */}
+          {/* <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
           />
-          <script async src="gtag.js" />
+          <script async src="gtag.js" /> */}
           {/* // TODO: Improve my meta description content (e.g. maybe match whatever I come up with for my Hero section) */}
           <meta
             name="description"
