dc85f737bd5b50d0e84fb5f698b7ca68b808faa5
Gary Xie <garyxie13@live.com>
2021-08-03 22:02:43 -0400

    Add scroll reveal for navbar

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-
+import { Fade } from 'react-awesome-reveal'
+import { useMediaQuery } from 'react-responsive'
 import HamburgerMenu from '@/components/HamburgerMenu/HamburgerMenu'
 import LogoTitle from '@/components/LogoTitle/LogoTitle'
 import ThemeToggle from '@/components/ThemeToggle/ThemeToggle'
@@ -7,6 +8,11 @@ import styles from './NavBar.module.scss'
 
 const NavBar: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState(false)
+  const isLargeNavSize = useMediaQuery({
+    query: '(min-width: 48em)',
+  })
+
+  console.log({ isLargeNavSize })
 
   return (
     <header className={styles.header}>
@@ -14,46 +20,58 @@ const NavBar: React.FC = () => {
         Skip to content
       </a>
       <div className={styles.navContainer}>
-        <LogoTitle />
-
+        <Fade triggerOnce duration={1000}>
+          <LogoTitle />
+        </Fade>
         <nav
           className={`${styles.navMenu} ${
             menuOpen ? styles.open : styles.closed
           }`}
         >
-          <ul>
-            <li>
-              <a href="#home">Home</a>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            {/* <li>
+          <Fade
+            direction="down"
+            cascade
+            triggerOnce
+            damping={0.2}
+            duration={isLargeNavSize ? 500 : 0}
+            delay={isLargeNavSize ? 300 : 0}
+          >
+            <ul>
+              <li>
+                <a href="#home">Home</a>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              {/* <li>
               <a href="#skills">Skills</a>
             </li> */}
-            {/* <li>
+              {/* <li>
               <a href="#experience">Experience</a>
             </li> */}
-            <li>
-              <a href="#projects">Projects</a>
-            </li>
-            <li>
-              <a href="#contact">Contact</a>
-            </li>
-            <li>
-              <a href="/resume.pdf" target="_blank" rel="noreferrer">
-                Resume
-              </a>
-            </li>
-          </ul>
+              <li>
+                <a href="#projects">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contact</a>
+              </li>
+              <li>
+                <a href="/resume.pdf" target="_blank" rel="noreferrer">
+                  Resume
+                </a>
+              </li>
+            </ul>
+          </Fade>
         </nav>
 
         <div className={styles.navBtns}>
           {/* <ThemeToggle /> */}
-          <HamburgerMenu
-            open={menuOpen}
-            onClick={() => setMenuOpen((prev) => !prev)}
-          />
+          <Fade triggerOnce duration={1000}>
+            <HamburgerMenu
+              open={menuOpen}
+              onClick={() => setMenuOpen((prev) => !prev)}
+            />
+          </Fade>
         </div>
       </div>
     </header>
