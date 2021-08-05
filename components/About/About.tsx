130e521a138f71c2ef3734819a9ead23168c4768
Gary Xie <garyxie13@live.com>
2021-08-04 23:01:51 -0400

    Update scroll reveal for About section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import SectionHeader from '@/components/SectionHeader/SectionHeader'
 import styles from './About.module.scss'
+import { Fade } from 'react-awesome-reveal'
 
 const About: React.FC = () => {
   return (
@@ -8,36 +9,42 @@ const About: React.FC = () => {
       <SectionHeader title="About Me" />
       <div className={styles.container}>
         <div className={styles.aboutText}>
-          <p>Hello, my name is Gary!</p>
-          <p>
-            I’m a software developer based in Toronto, Canada, and I am
-            currently in my 3rd year of studying Computer Science at University
-            of Toronto.
-          </p>
-          <p>
-            I’ve been lucky enough to experience a variety of software
-            development internships during my undergraduate studies. I have
-            previously interned at{' '}
-            <a href="https://verto.health/" target="_blank" rel="noreferrer">
-              Verto
-            </a>
-            , and I am currently finishing up a 16-month internship at{' '}
-            <a href="https://www.ibm.com/" target="_blank" rel="noreferrer">
-              IBM
-            </a>
-            .
-          </p>
+          <Fade triggerOnce cascade direction="up" damping={0.5} duration={500}>
+            <p>Hello, my name is Gary!</p>
+            <p>
+              I’m a software developer based in Toronto, Canada, and I am
+              currently in my 3rd year of studying Computer Science at
+              University of Toronto.
+            </p>
+            <p>
+              I’ve been lucky enough to experience a variety of software
+              development internships during my undergraduate studies. I have
+              previously interned at{' '}
+              <a href="https://verto.health/" target="_blank" rel="noreferrer">
+                Verto
+              </a>
+              , and I am currently finishing up a 16-month internship at{' '}
+              <a href="https://www.ibm.com/" target="_blank" rel="noreferrer">
+                IBM
+              </a>
+              .
+            </p>
+          </Fade>
         </div>
         <div className={styles.skills}>
-          Here are a few technologies I’ve been working with recently:
-          <ul>
-            <li>TypeScript</li>
-            <li>React</li>
-            <li>Next.js</li>
-            <li>Golang</li>
-            <li>Node.js</li>
-            <li>MongoDB</li>
-          </ul>
+          <Fade triggerOnce direction="up" duration={500} delay={1000}>
+            <p>Here are a few technologies I’ve been working with recently:</p>
+          </Fade>
+          <Fade triggerOnce cascade direction="up" duration={500} delay={1500}>
+            <ul>
+              <li>TypeScript</li>
+              <li>React</li>
+              <li>Next.js</li>
+              <li>Golang</li>
+              <li>Node.js</li>
+              <li>MongoDB</li>
+            </ul>
+          </Fade>
           {/* // TODO: Maybe merge skills section into here as a second column, so that I have something to take up horizontal space in About for? (or take a photo of myself?)*/}
         </div>
       </div>
