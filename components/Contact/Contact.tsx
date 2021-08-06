17804dc64d8300dc05e2ffe0d627f7e69dd86ca5
Gary Xie <garyxie13@live.com>
2021-08-05 20:45:30 -0400

    Animate Contact

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -2,34 +2,37 @@ import React, { useState } from 'react'
 import SectionHeader from '@/components/SectionHeader/SectionHeader'
 import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa'
 import styles from './Contact.module.scss'
+import { Fade } from 'react-awesome-reveal'
 
 // TODO: Eventually add an EmailJS form to the Contact section
 const Contact: React.FC = () => {
   return (
     <div className={styles.contact}>
-      <SectionHeader title="Contact Me" />
-      <div className={styles.container}>
-        <a href="https://github.com/GaryJX" target="_blank" rel="noreferrer">
-          <FaGithub />
-          GaryJX
-        </a>
-        <a
-          href="https://www.linkedin.com/in/GaryJX"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaLinkedin />
-          GaryJX
-        </a>
-        <a
-          href="mailto:gary.xie@mail.utoronto.ca"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FaEnvelope />
-          gary.xie@mail.utoronto.ca
-        </a>
-      </div>
+      <Fade triggerOnce direction="up">
+        <SectionHeader title="Contact Me" />
+        <div className={styles.container}>
+          <a href="https://github.com/GaryJX" target="_blank" rel="noreferrer">
+            <FaGithub />
+            GaryJX
+          </a>
+          <a
+            href="https://www.linkedin.com/in/GaryJX"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedin />
+            GaryJX
+          </a>
+          <a
+            href="mailto:gary.xie@mail.utoronto.ca"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaEnvelope />
+            gary.xie@mail.utoronto.ca
+          </a>
+        </div>
+      </Fade>
     </div>
   )
 }
