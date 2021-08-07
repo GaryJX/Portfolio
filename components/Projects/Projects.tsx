14d6e371725067f20ae7f78ced308f5a5dcd70be
Gary Xie <garyxie13@live.com>
2021-08-07 17:49:28 -0400

    Improve responsiveness

--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -8,7 +8,8 @@ const projects: ProjectProps[] = [
   // {},
   // {
   //   title: 'Video Chat App',
-  //   description: 'TODO: description',
+  //   description:
+  //     'A web app for finding your public information based on IP address including ISP, location, and time zone.',
   //   technologies: ['Next.js', 'React'],
   //   github: 'https://github.com/GaryJX/<todo>',
   //   demo: 'https://<todo>.vercel.app',
