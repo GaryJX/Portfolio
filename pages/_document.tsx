ac372c514255a2597ee71fd8ecd2311ca883662a
Gary Xie <garyxie13@live.com>
2021-08-08 09:57:31 -0400

    Clean up todos

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,13 +5,6 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          {/* // TODO: Uncomment this later to include Google Analytics (the issue is that it is getting blocked by ad blockers right now) */}
-          {/* <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-          />
-          <script async src="gtag.js" /> */}
-          {/* // TODO: Improve my meta description content (e.g. maybe match whatever I come up with for my Hero section) */}
           <meta
             name="description"
             content="Gary Xie is a software developer specializing in building extraordinary experiences for the web."
