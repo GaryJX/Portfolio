a82b090e20b95e8ccbc173aebf775b0531e1024c
Gary Xie <garyxie13@live.com>
2021-08-08 09:55:20 -0400

    Clean up TODO note

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,7 +19,6 @@ const App: React.FC<AppProps> = ({ Component, pageProps }) => {
           content="height=device-height, width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5, viewport-fit=cover, shrink-to-fit=no"
         />
         {/* // TODO: Add meta tags for better SEO, and maybe PWA eventually? */}
-        {/* // TODO: Add Google Analytics for my production site to see how many users visit, and from what devices? */}
         {[400, 500, 600, 700].map((weight) => (
           <link
             key={weight}
